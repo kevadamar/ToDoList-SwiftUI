7c065d5c617abb9ec29fae13583aee99879ee135
keva damar galih <kevadamarg@gmail.com>
2021-04-28 20:10:16 +0700

    Initial Commit

---
+++ SwiftUIToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIToDoList
+//
+//  Created by keva damar galih on 28/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
